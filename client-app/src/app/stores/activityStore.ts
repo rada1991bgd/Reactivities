979aa7fb36d4f9b481f331994b8a25625840b71a
rada1991bgd <rada1991bgd@gmail.com>
2022-01-19 11:58:38 +0100

    End of section 9

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -19,6 +19,16 @@ export default class ActivityStore {
     );
   }
 
+  get groupedActivities() {
+    return Object.entries(
+      this.activitiesByDate.reduce((activities, activity) => {
+        const date = activity.date;
+        activities[date] = activities[date] ? [...activities[date], activity] : [activity];
+        return activities;
+      }, {} as {[key: string]: Activity[]})
+    );
+  }
+
   loadActivities = async () => {
     this.loadingInitial = true;
     try {
@@ -43,9 +53,9 @@ export default class ActivityStore {
       try {
         activity = await agent.Activities.details(id);
         this.setActivity(activity);
-        runInAction(()=>{
+        runInAction(() => {
           this.selectedActivity = activity;
-        })
+        });
         this.setLoadingInitial(false);
         return activity;
       } catch (error) {
