4932e2c7047173a987c69d3c087d0ca0682d23f3
rada1991bgd <rada1991bgd@gmail.com>
2022-01-28 17:09:11 +0100

    End of section 13

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -14,14 +14,14 @@ export default observer(function ActivityDashboard() {
     if(activityRegistry.size <= 1) loadActivities();
   }, [activityRegistry.size, loadActivities]);
 
-  if (activityStore.loadingInitial) return <LoadingComponent content="Loading app" />;
+  if (activityStore.loadingInitial) return <LoadingComponent content="Loading activities..." />;
   return (
     <Grid>
       <Grid.Column width="10">
         <ActivityList/>
       </Grid.Column>
       <Grid.Column width="6">
-        <ActivityFilters />
+        <ActivityFilters /  >
       </Grid.Column>
     </Grid>
   );
