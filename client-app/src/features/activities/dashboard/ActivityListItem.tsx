b55f76cbc87f74f9be9d1713a720269f9873c74f
rada1991bgd <rada1991bgd@gmail.com>
2022-01-31 17:57:31 +0100

    end of section 17

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -26,25 +26,44 @@ export default function ActivityListItem({ activity }: Props) {
   return (
     <Segment.Group>
       <Segment>
-        {activity.isCanceled && 
-        <Label attached='top' color='red' content='Cancelled' style={{textAlign: 'center'}} />
-        }
+        {activity.isCanceled && (
+          <Label
+            attached="top"
+            color="red"
+            content="Cancelled"
+            style={{ textAlign: "center" }}
+          />
+        )}
         <Item.Group>
           <Item>
-            <Item.Image style={{marginBottom: 3}} size="tiny" circular src="/assets/user.png" />
+            <Item.Image
+              style={{ marginBottom: 3 }}
+              size="tiny"
+              circular
+              src={activity.host?.image || "/assets/user.png"}
+            />
             <Item.Content>
               <Item.Header as={Link} to={`/activities/${activity.id}`}>
                 {activity.title}
               </Item.Header>
-              <Item.Description>Hosted by {activity.host?.displayName}</Item.Description>
+              <Item.Description>
+                Hosted by{" "}
+                <Link to={`/profiles/${activity.host?.username}`}>
+                  {activity.host?.displayName}
+                </Link>
+              </Item.Description>
               {activity.isHost && (
                 <Item.Description>
-                  <Label basic color='orange'>You are hosting this activity</Label>
+                  <Label basic color="orange">
+                    You are hosting this activity
+                  </Label>
                 </Item.Description>
               )}
               {activity.isGoing && !activity.isHost && (
                 <Item.Description>
-                  <Label basic color='green'>You are going to this activity</Label>
+                  <Label basic color="green">
+                    You are going to this activity
+                  </Label>
                 </Item.Description>
               )}
             </Item.Content>
@@ -53,12 +72,12 @@ export default function ActivityListItem({ activity }: Props) {
       </Segment>
       <Segment>
         <span>
-          <Icon name="clock" /> {format(activity.date!, 'dd MMM yyyy h:mm aa')}
+          <Icon name="clock" /> {format(activity.date!, "dd MMM yyyy h:mm aa")}
           <Icon name="marker" /> {activity.venue}
         </span>
       </Segment>
       <Segment secondary>
-        <ActivityListItemAttendee attendees={activity.attendees!}/>
+        <ActivityListItemAttendee attendees={activity.attendees!} />
       </Segment>
       <Segment clearing>
         <span>{activity.description}</span>
