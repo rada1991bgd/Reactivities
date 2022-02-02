c29ebdd1b470755b0d3c7f681c0bb5235319e2b2
rada1991bgd <rada1991bgd@gmail.com>
2022-02-02 16:59:01 +0100

    End of section 20

--- Application/Profiles/Details.cs
+++ Application/Profiles/Details.cs
@@ -1,14 +1,12 @@
-﻿using Application.Core;
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Core;
+using Application.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.Profiles
 {
@@ -18,22 +16,28 @@ namespace Application.Profiles
         {
             public string Username { get; set; }
         }
+
         public class Handler : IRequestHandler<Query, Result<Profile>>
         {
             private readonly DataContext _context;
             private readonly IMapper _mapper;
-
-            public Handler(DataContext context, IMapper mapper)
+            private readonly IUserAccessor _userAccessor;
+            public Handler(DataContext context, IMapper mapper, IUserAccessor userAccessor)
             {
-                _context = context;
+                _userAccessor = userAccessor;
                 _mapper = mapper;
+                _context = context;
             }
 
             public async Task<Result<Profile>> Handle(Query request, CancellationToken cancellationToken)
             {
                 var user = await _context.Users
-                    .ProjectTo<Profile>(_mapper.ConfigurationProvider)
+                    .ProjectTo<Profile>(_mapper.ConfigurationProvider,
+                        new { currentUsername = _userAccessor.GetUsername() })
                     .SingleOrDefaultAsync(x => x.Username == request.Username);
+
+                if (user == null) return null;
+
                 return Result<Profile>.Success(user);
             }
         }
--- client-app/src/features/activities/details/ActivityDetailedSidebar.tsx
+++ client-app/src/features/activities/details/ActivityDetailedSidebar.tsx
@@ -41,7 +41,8 @@ export default observer(function ActivityDetailedSidebar({ activity: {attendees,
                     {attendee.displayName}
                   </Link>
                 </Item.Header>
-                <Item.Extra style={{ color: "orange" }}>Following</Item.Extra>
+                {attendee.following && 
+                <Item.Extra style={{ color: "orange" }}>Following</Item.Extra>}
               </Item.Content>
             </Item>
           ))}
